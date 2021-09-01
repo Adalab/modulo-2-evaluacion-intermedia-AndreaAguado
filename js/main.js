c9426b8ce58db496a579a92d34041118ba7b03cb
AndreaAguado <andrea95am@gmail.com>
2021-09-01 09:28:59 +0200

    first version of handle

--- js/main.js
+++ js/main.js
@@ -2,12 +2,30 @@
 
 const input = document.querySelector('.js_input');
 const button = document.querySelector('.js_button');
+const hint = document.querySelector('.js_hint');
+
+const number = getRandomNumber(100);
+console.log(number);
 
 function handleButton () {
     console.log('estoy dentro');
-    if (input.innerHTML < 1 || input.innerHTML > 100){
-        console.log('numero invalido');
+    // const number = getRandomNumber(100);
+    // console.log(number);
+    console.log(input.value);
+    console.log(typeof input.value);
+    if (input.value < 1 || input.value > 100){
+        hint.innerHTML = 'El número debe estar entre 1 y 100';
     }
+    else if (input.value > number) {
+        hint.innerHTML = 'Demasiado alto';
+    }
+    else if (input.value < number){
+        hint.innerHTML = 'Demasiado bajo';
+    }
+    else if (input.value === number){
+        hint.innerHTML = 'Has ganado campeona!!!';
+    }
+
 }
 
 function getRandomNumber(max) {
