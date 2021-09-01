de66b23441aa5fa194444110a8e1cd362f43defc
AndreaAguado <andrea95am@gmail.com>
2021-09-01 09:13:08 +0200

    add handle and randomizer in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const input = document.querySelector('.js_input');
+const button = document.querySelector('.js_button');
+
+function handleButton () {
+    console.log('estoy dentro');
+    if (input.innerHTML < 1 || input.innerHTML > 100){
+        console.log('numero invalido');
+    }
+}
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+button.addEventListener('click', handleButton);
